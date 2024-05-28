1fcd7373880eaf33016fe0af68d10ab82b657984
Ilya Goncharov <ilya.goncharov@jetbrains.com>
2024-05-28 16:16:07 +0200

    Adopt for user projects with version catalog

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,9 +1,26 @@
 rootProject.name = "kotlin-wasm-wasi-example"
 
 pluginManagement {
+    val kotlin_repo_url: String? by settings
+
     resolutionStrategy {
         repositories {
             gradlePluginPortal()
+
+            kotlin_repo_url?.also { maven(it) }
         }
     }
 }
+
+dependencyResolutionManagement {
+    val kotlin_version: String? by settings
+
+    versionCatalogs {
+        create("libs") {
+            kotlin_version?.let {
+                version("kotlin", it)
+            }
+        }
+    }
+}
+
