2530883597e8b4abc36461ce02c4547baf5f0c7d
Zalim Bashorov <zalim.bashorov@jetbrains.com>
2024-10-13 21:42:56 +0200

    Add instructions on how to turn off using EH and how to switch to the final version of the EH proposal.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,8 @@
+@file:OptIn(ExperimentalWasmDsl::class)
+
 import org.gradle.internal.os.OperatingSystem
 import de.undercouch.gradle.tasks.download.Download
+import org.jetbrains.kotlin.gradle.targets.js.dsl.ExperimentalWasmDsl
 import org.jetbrains.kotlin.gradle.targets.js.nodejs.NodeJsExec
 import org.jetbrains.kotlin.gradle.targets.js.testing.KotlinJsTest
 import org.jetbrains.kotlin.gradle.testing.internal.KotlinTestReport
@@ -29,6 +32,20 @@ kotlin {
     }
 }
 
+// Uncomment following block to turn off using the Exception Handling proposal.
+// Note, with this option, the compiler will generate `unreachable` instruction instead of throw, 
+// and a Wasm module will stop execution in this case.
+//
+// tasks.withType<org.jetbrains.kotlin.gradle.dsl.KotlinJsCompile>().configureEach {
+//     compilerOptions.freeCompilerArgs.addAll(listOf("-Xwasm-use-traps-instead-of-exceptions"))
+// }
+
+// Uncomment following block to force using the final version of the Exception Handling proposal.
+// Note, the new opcodes are not supported yet in WAMR and Node.js
+//
+// tasks.withType<org.jetbrains.kotlin.gradle.dsl.KotlinJsCompile>().configureEach {
+//     compilerOptions.freeCompilerArgs.addAll(listOf("-Xwasm-use-new-exception-proposal"))
+// }
 
 enum class OsName { WINDOWS, MAC, LINUX, UNKNOWN }
 enum class OsArch { X86_32, X86_64, ARM64, UNKNOWN }
